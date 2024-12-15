76d9edc17becb9656584143417061991c676d650
thion96 <thion96@mail.ru>
2024-12-15 17:26:03 +0300

    create 2 thread

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <thread>
+#include <iostream>
+
+void threadFunction1()
+{
+    std::cout << "Поток 1 с обычной функцией без аргументов\n";
+}
+
+void threadFunction2(int i, double d)
+{
+    std::cout << "Поток 2 с обычной функцией с аргументами: " << i << ' ' << d << '\n';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::thread myThread1(threadFunction1);
+    myThread1.join();
+
+    std::thread myThread2(threadFunction2, 1, 2.34);
+    myThread2.join();
+
+    return 0;
+}
